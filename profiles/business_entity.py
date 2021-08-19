9d2148f142e317faec0b92ee5c30abf2b839c430
Dillon Stock <stockdillon@yahoo.com>
2021-08-19 02:34:39 -0400

    add list properties

--- profiles/business_entity.py
+++ profiles/business_entity.py
@@ -1,3 +1,5 @@
 class BusinessEntity(object):
     def __init__(self, name="BusinessEntity"):
         self.name = name
+        self.repositories = []
+        self.projects = []
--- profiles/github/organization.py
+++ profiles/github/organization.py
@@ -3,3 +3,5 @@ from ..business_entity import BusinessEntity
 class Organization(BusinessEntity):
     def __init__(self, name="Organization"):
         super().__init__(name)
+        self.packages = []
+        self.people = []
